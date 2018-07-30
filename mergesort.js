613694114c9834e74d5a7895d341a9e64e2fc42d
Minkyu Yang <parigu2@msn.com>
2018-07-30 14:14:32 -0500

    mergeSort

--- mergesort.js
+++ mergesort.js
@@ -1,3 +1,12 @@
 function split(arr) {
+    var firstHalf = arr.slice(0, Math.ceil((arr.length/2)))
+    var secondHalf = arr.slice(Math.ceil((arr.length/2)))
+
     return [firstHalf, secondHalf];
 }
+
+function merge(array) {
+    /* your code here */
+    return array.reduce((acc, val) =>
+        acc.concat(val),[]);
+  }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,7 +4,18 @@ describe('Split Array function', function() {
       expect(split([1,2])).toEqual([[1],[2]]);
     });
 
-    it('is able to split an array with add amount of array into two halves', function() {
-        expect(split([1,2,3]).toEqual([[1,2],[3]]);
+    it('is able to split an array with odd amount of array into two halves', function() {
+        expect(split([1,2,3])).toEqual([[1,2],[3]]);
     })
 });
+
+describe('Merge function', function(){
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      // test the merging algorithm
+      expect(merge([[1],[2]])).toEqual([1,2])
+    });
+    it('is able to merge two sorted arrays with odd amount into one sorted array', function(){
+        // test the merging algorithm
+        expect(merge([[1,2],[3]])).toEqual([1,2,3])
+      });
+  });
