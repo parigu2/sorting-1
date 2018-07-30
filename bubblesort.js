00db16ffdaf861594e4542677d2fbd05b84569a7
Minkyu Yang <parigu2@msn.com>
2018-07-30 11:13:15 -0500

    spy on

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,27 @@
 function bubbleSort(array) {
-    
+    if (array.length < 2) {
+        return array;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        var bigger; 
+        var smaller;
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j+1]) {
+                bigger = array[j]
+                smaller = array[j+1]
+                array[j] = smaller;
+                array[j+1] = bigger;
+            }
+        }
+    }
+    return array;
 }
+
+// function swap(num1, num2) {
+//     const first = num1;
+//     const second = num2;
+//     if (first > second) {
+//         num1 = second;
+//     }
+// }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -8,6 +8,6 @@ describe('Bubble Sort', function(){
   });
 
   it('handles multiple item', function() {
-    expect( bubbleSort([4, 2, 3, 5, 1] ).toEqual( [1, 2, 3, 4, 5] ));
+    expect( bubbleSort([4, 2, 3, 5, 1]) ).toEqual( [1, 2, 3, 4, 5] );
   })
 });
